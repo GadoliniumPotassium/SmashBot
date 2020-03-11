b8ab58dd2657418a85a0d929e27e6771aa65a423
GadoliniumPotassium <gdkh514@gmail.com>
2020-03-11 14:34:06 -0400

    More details added

--- ReactionFunctions.py
+++ ReactionFunctions.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-document=openpyxl.load_workbook()
+document=openpyxl.load_workbook("").get_sheet_by_name("Sheet1")
 
 def win(message,client,args):
     return 0==0
@@ -9,4 +9,13 @@ def loss(message,client,args):
     return 0==0
 
 def rankList(message,client,args):
+    return 0==0
+
+def displayRecord(message,client,args):
+    return 0==0
+
+def addToDB(message,client,args):
+    return 0==0
+
+def setMain(message,client,args):
     return 0==0
