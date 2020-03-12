5089b0560e4c23791ed30bf9ade587c61e03a56f
GadoliniumPotassium <gdkh514@gmail.com>
2020-03-12 13:03:25 -0400

    ALL IS GOOD

--- Falcon.py
+++ Falcon.py
@@ -1,6 +1,5 @@
 #bot.py
 import discord
-import EventHandler
 import ReactionFunctions
 import SheetsController
 from dotenv import load_dotenv
@@ -9,7 +8,7 @@ TOKEN = 'Njg3MzEwMjUwMjY0NDk0MTA1.Xmj7aw.rT9i1xA1O2raTdledApzfAOYtsU'
 GUILD = '687313651216285811'
 client = discord.Client()
 
-EH=EventHandler
+RF=ReactionFunctions
 
 @client.event
 async def on_ready():
@@ -24,13 +23,38 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	# print message content
-	message_content=message.content
-	message_sender=message.author.name
-	message_sender_id=message.author.id
-	message_sender_id=message_sender_id
-	print(str(message_content)+"\n"+str(message_sender)+"\n"+str(message_sender_id))
-	#await message.content
+	if message.author == client.user:
+		pass
+	else:
+		channel=message.channel
+		# print message content
+		message_content=message.content
+		message_sender=message.author.name
+		message_sender_id=message.author.id
+		message_sender_id=message_sender_id
+		tokens= message_content.split(" ")
+		tokens = list(filter(None, tokens))
+		if(tokens[0]=="!falcon" and len(tokens)>1):
+			if(tokens[1]=="help"):
+				print(RF.listCommands())
+				await channel.send(RF.listCommands())
+			elif(tokens[1]=="rank"):
+				print(RF.rankList())
+				await channel.send(RF.rankList())
+			elif(tokens[1]=="members"):
+				print(RF.listLeagueMembers())
+				await channel.send(RF.listLeagueMembers())
+			elif(tokens[1]=="record"):
+				print(RF.displayRecord([message.author.name]))
+				await channel.send(RF.displayRecord([message.author.name]))
+			elif(tokens[1]=="win" or tokens[1]=="loss"):
+				await channel.send(RF.win_loss(tokens, message.author.name))
+			elif (tokens[1]=="join"):
+				await channel.send(RF.joinLeague([message.author.name,message.author.id]))
+		else:
+			await channel.send("This is not a valid command, try again or type :'!falcon help' for more information")
+			
+
 
 # start bot
 client.run(TOKEN)
--- ReactionFunctions.py
+++ ReactionFunctions.py
@@ -2,29 +2,39 @@ import SheetsController
 
 document=SheetsController.getSheet()
 
-
-def win_loss(message,client,args):
+def win_loss(message_split,args):
     #find discord ID in spreadsheet
     #go to win column, get the data and add plus one
     #go to the other person mentioned and go to their loss sheet and add one to their loss sheet
     #time to define args 
     #args[0] is the sender
-    
-    message_split=message.split(" ")
-    message_split[2]=message_split[2][3:-1]
-    OPcell=document.find(message_split[2])
-    UserCell=document.find(args[0])
-    if(message_split[0].lower="win"):
-        document.update_cell(UserCell.row,UserCell.col+2,int(UserCell.value)+1)
-        document.update_cell(OPcell.row,OPcell.col+2,int(OPcell.value)+1)
-        return True
-    elif(args[1] == "loss"):
-        document.update_cell(UserCell.row,UserCell.col+3,int(UserCell.value)+1)
-        document.update_cell(OPcell.row,OPcell.col+1,int(OPcell.value)+1)
-        return True
-    return 0==0
 
-def rankList(message,client,args):
+    message_split[2]=message_split[2][3:-1]
+    try:
+        OPcell=document.find(message_split[2])
+        UserCell=document.find(args[0])
+    except Exception as e:
+        print (e)
+        return "One of the two people are not in the league, join league to play"
+    
+    print("about to enter if")
+    if(message_split[0].lower() == "win"):
+        win=str(document.cell(UserCell.row,UserCell.col+2).value)
+        loss=str(document.cell(OPcell.row,OPcell.col+2).value)
+        document.update_cell((UserCell.row),(UserCell.col+2),str(int(win)+1))
+        document.update_cell(OPcell.row,OPcell.col+2,str(int(loss)+1))
+        return displayRecord(args[0],message_split[2])
+        
+    elif(message_split[0].lower() == ("loss")):
+        win=str(document.cell(UserCell.row,UserCell.col+2).value)
+        loss=str(document.cell(OPcell.row,OPcell.col+2).value)
+        document.update_cell(OPcell.row,OPcell.col+2,str(int(loss)+1))
+        document.update_cell((UserCell.row),(UserCell.col+2),str(int(win)+1))
+        return displayRecord(args[0],message_split[2])
+        
+    return True
+
+def rankList(args):
     #Will sort through all the players and print them
     # in order of highest to lowest Win/Loss Ratio
     stat_list={}
@@ -47,40 +57,50 @@ def rankList(message,client,args):
     
     return final_str
 
-def displayRecord(message,client,args):
+def displayRecord(args):
     #Will display the record of the calling person
-    #Will go in by his user ID or username and print out his
+    #Will go in by his username or id and print out his
     #win loss record as well as his W/L ratio
     str_result=""
-    max_cols=len(document.col_values(1))
-    for i in range(max_cols+1):
-        if(i>1):
-            #We are getting the username
-            if(document.cell(i,1).value == args[0]):
-                str_result="The record of "+str(args[0])+" record is "+ str(document.cell(i,3).value) +" wins and " + str(document.cell(i,3).value) +" losses and he has a Win-Loss ratio of "+ str(document.cell(i,4).value) +"."
-            
+    try:
+        usersCell=document.find(args[0])
+    except Exception as e:
+        return "You are not in the league, please join to have a record."
+
+    row=usersCell.row
+    col=usersCell.col
+    if(usersCell.col==2):
+        str_result="The record of "+document.cell(row,col-1).value+" is "+ document.cell(row,3).value+" wins and " + document.cell(row, 4).value+" losses for a Win-Loss ratio of "+ document.cell(row,5).value
+    if(usersCell.col==1):
+        str_result="The record of "+document.cell(row,col).value+" is "+ document.cell(row,3).value+" wins and " + document.cell(row, 4).value+" losses for a Win-Loss ratio of "+ document.cell(row,5).value
     return str_result
 
-def joinLeague(message,client,args):
+def joinLeague(args):
     #will check to see if user is already in database
     #if he is, then return false and go home
     #if he isn't go to the bottom of the list and create a new cell for him.
     row_where_to_add=len(document.col_values(1)) + 1
     column = len(document.row_values(1))
-    #args 0 is the username
-    document.update_cell(row_where_to_add, 1, args[0])
-    #args 1 is the user id
-    document.update_cell(row_where_to_add, 2, args[1])
-    document.update_cell(row_where_to_add, 3,0)
-    document.update_cell(row_where_to_add, 4,0)
-    document.update_cell(row_where_to_add, 5,'=IF(D7=0,"Perfect Run",C7/D7)')
-    
-    return True
+    try:
+        document.find(args[0])
+        return "Player already exists in the league, cannot join again"
+    except Exception as e:
+          #args 0 is the username
+        document.update_cell(row_where_to_add, 1, args[0])
+        #args 1 is the user id
+        document.update_cell(row_where_to_add, 2, args[1])
+        document.update_cell(row_where_to_add, 3,0)
+        document.update_cell(row_where_to_add, 4,0)
+        document.update_cell(row_where_to_add, 5,'=IF(D7=0,"Perfect Run",C7/D7)')
+    return args[0]+" has joined the league"
 
-def listLeagueMembers(message,client,args):
+def listLeagueMembers():
      max_cols=len(document.col_values(1))
      final_str="Here are the current members of the league:\n"
      for i in range(max_cols+1):
          if(i>1):
              final_str+=(str(document.cell(i,1).value)+"\n")
      return final_str
+
+def listCommands():
+    return "command_list = Here are the following commands:\n!falcon: {\nrank\nmembers\n'win vs @member'\n'loss vs @member'\nrecord\n}"
