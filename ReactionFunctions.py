7ee2a6e440722a74e24e0a014c9a74f940a9bb82
GadoliniumPotassium <gdkh514@gmail.com>
2020-04-22 15:25:45 -0400

    Changed the entire infrastructure

--- ReactionFunctions.py
+++ ReactionFunctions.py
@@ -1,139 +1,147 @@
-import SheetsController
+import openpyxl
 
-document=SheetsController.getSheet()
-def win_loss(me,split_command):
-    #find discord ID in spreadsheet
-    #go to win column, get the data and add plus one
-    #go to the other person mentioned and go to their loss sheet and add one to their loss sheet
-    #time to define args 
-    #me is the sender
-    Foundhim=False
-    Foundme=False
-    rowMe=0
-    colMe=0
-    rowHim=0
-    colHim=0
-    max_cols=len(document.col_values(1))
+workFile = openpyxl.load_workbook("Stats.xlsx", data_only=True)
+document = workFile.active
+
+
+def winOrLoss(me, split_command):
+    # find discord ID in spreadsheet
+    # go to win column, get the data and add plus one
+    # go to the other person mentioned and go to their loss sheet and add one to their loss sheet
+    # time to define args
+    # me is the sender
+    Foundhim = False
+    Foundme = False
+    max_height = document.max_row
+    max_width = document.max_column
+    rowMe = 0
+    colMe = 0
+    rowHim = 0
+    colHim = 0
     for char in split_command[3]:
-    	if char in "<>@!":
-        	split_command[3]=split_command[3].replace(char,'')
-    
-    for i in range(max_cols+1):
-        if(i>1):
-            if(str(document.cell(i,2).value)==str(me) and Foundhim == False):
-                print("Found the calling user")
-                Foundhim=True
-                rowMe=i
-                colMe=1
-            if(str(document.cell(i,2).value)==str(split_command[3]) and Foundme == False):
-                print("Found the opponent")
-                Foundme=True
-                rowHim=i
-                colHim=1
-
-    if(Foundhim and Foundme):
+        if char in "<>@!":
+            split_command[3] = split_command[3].replace(char, '')
+    for i in range(2, max_height + 1):
+        if Foundme == False and str(document.cell(row=i, column=2).value) == str(me):
+            rowMe = i
+            colMe = 1
+            Foundme = True
+        if Foundhim == False and str(document.cell(row=i, column=2).value) == str(split_command[3]):
+            rowHim = i
+            colHim = 1
+            Foundhim = True
+    if Foundhim and Foundme:
         print("Both people were found")
-        #cell update begins here
-        if(split_command[1].lower()=="win"):
+        # This is where the updating happened
+        if (split_command[1].lower() == "win"):
             print("updating in win")
-            document.update_cell(rowMe,colMe+2,str(int(document.cell(rowMe,colMe+2).value)+1))
-            document.update_cell(rowHim,colHim+3 ,str(int(document.cell(rowHim,colHim+3).value)+1))
-            print("Data has been updated")
-            return "Data has been updated, to see record enter '!falcon record'."
-        elif(split_command[1].lower()=="loss"):
+            document.cell(row=rowMe, column=colMe + 2).value = str(
+                int(document.cell(row=rowMe, column=colMe + 2).value) + 1)
+            document.cell(row=rowHim, column=colHim + 3).value = str(
+                int(document.cell(row=rowHim, column=colMe + 3)) + 1)
+            workFile.save("Stats.xlsx")
+            return "Data has been updated, please do !falcon record to see your new record"
+        elif split_command[1].lower() == "loss":
             print("updating in loss")
-            document.update_cell(rowMe,colMe+3,str(int(document.cell(rowMe,colMe+3).value)+1))
-            document.update_cell(rowHim,colHim+2,str(int(document.cell(rowHim,colHim+2).value)+1))
-            print("Data has been updated")
-            return "Data has been updated, to see record enter '!falcon record'."
+            document.cell(row=rowMe, column=colMe + 3).value = str(
+                int(document.cell(row=rowMe, column=colMe + 3).value) + 1)
+            document.cell(row=rowHim, column=colHim + 2).value = str(
+                int(document.cell(row=rowHim, column=colMe + 2)) + 1)
+            workFile.save("Stats.xlsx")
+            return "Data has been updated, please do !falcon record to see your new record"
         else:
-            print ("Something wrong")
-            return "Something wrong"
+            print("Something is wrong")
+            return False
     else:
-        return "One of you two is not there, make sure you're in the league."
-        
+        return "One of you 2 is not in the league, please make sure you both appear"
+
+
+# Will sort through all the players and print them
+# in order of highest to lowest Win/Loss Ratio
 def rankList(args):
-    #Will sort through all the players and print them
-    # in order of highest to lowest Win/Loss Ratio
-    stat_list={}
-    max_cols=len(document.col_values(1))
-    for i in range(max_cols+1):
-        if(i>1):
-            #We are getting the username 
-            stat_list[str(document.cell(i,1).value)]=document.cell(i,5).value
-    a=sorted(stat_list, key=stat_list.get)
+    stat_list = {}
+    max_height = document.max_row
+    for i in range(2, max_height + 1):
+        stat_list[document.cell(row=i, column=1).value] = str(document.cell(row=i, column=5).value)
+    # We will sort the stat list here
+    a = sorted(stat_list, key=stat_list.get)
     a.reverse()
-    pos=1
-    final_str="Rankings:\n"
+    print(a)
+    pos = 1
+    final_str = "Rankings:\n"
     for i in a:
-        final_str += str(pos)+" -> "+str(i)+" : "+str(stat_list[i])+"\n"
-        #When we find your position, we will append that message to the beginning of the list and say something
-        #of the like, we found you at position number x. 
-        if(i == args[0]):
-            final_str="You were found at position number "+str(pos)+"\n" + final_str
-        pos+=1
-    
+        final_str += str(pos) + " -> " + i + " : " + stat_list[i] + "\n"
+        # When we find your position, we will append that message to the beginning of the list and say something
+        # of the like, we found you at position number x.
+        if (i == args[0]):
+            final_str = "You were found at position number " + str(pos) + "\n" + final_str
+            break
+        pos += 1
     return final_str
+
 
 def displayRecord(args):
-    #Will display the record of the calling person
-    #Will go in by his ID and print out his
-    #win loss record as well as his W/L ratio
-    str_result=""
-    isThere=False
-    row=0
-    col=0
-    max_cols=len(document.col_values(1))
-    for i in range(max_cols+1):
-        if(i>=2):
-            if(args[0]==document.cell(i,1).value):
-                row=i
-                col=1
-                isThere=True
-    for i in range(max_cols+1):
-        if(i>=2):
-            if(args[0]==document.cell(i,2).value):
-                row=i
-                col=2
-                isThere=True
-                    
+    # Will display the record of the calling person
+    # Will go in by his ID and print out his
+    # win loss record as well as his W/L ratio
+    str_result = ""
+    isThere = False
+    row = 0
+    col = 0
+    max_height = document.max_row
+    for i in range(2, max_height + 1):
+
+        if (args[0] == document.cell(row=i, column=1).value):
+            row = i
+            col = 1
+            isThere = True
+            break
+    for i in range(2, max_height + 1):
+        if (args[0] == document.cell(row=i, column=2).value):
+            row = i
+            col = 2
+            isThere = True
+            break
     if isThere:
-        if(col==2):
-            str_result="The record of " + document.cell(row,col-1).value+ " is " + document.cell(row,3).value+ " wins and " + document.cell(row, 4).value+" losses for a Win-Loss ratio of "+ document.cell(row,5).value
-        elif(col==1):
-            str_result="The record of "+document.cell(row,col).value+" is "+ document.cell(row,3).value+" wins and " + document.cell(row, 4).value+" losses for a Win-Loss ratio of "+ document.cell(row,5).value
+        if col == 1:
+            str_result = "The record of " + str(document.cell(row=row, column=col).value) + " is " + str(
+                document.cell(row=row, column=3).value) + " wins and " + str(
+                document.cell(row=row, column=4).value) + " losses for a Win-Loss ratio of " + str(
+                document.cell(row=row, column=5).value)
+        elif col == 2:
+            str_result = "The record of " + str(document.cell(row, col - 1).value) + " is " + str(
+                document.cell(row, 3).value) + " wins and " + str(
+                document.cell(row, 4).value) + " losses for a Win-Loss ratio of " + str(document.cell(row, 5).value)
         return str_result
     else:
-        return "One of the two players are not there"
+        return "You are not in the league"
+
 
 def joinLeague(args):
-    #will check to see if user is already in database
-    #if he is, then return false and go home
-    #if he isn't go to the bottom of the list and create a new cell for him.
-    row_where_to_add=len(document.col_values(1)) + 1
-    column = len(document.row_values(1))
-    try:
-        document.find(args[0])
-        return "Player already exists in the league, cannot join again"
-    except Exception as e:
-        fh=open("members.txt","a")
-        fh.write(str(args[0]))
-        fh.close()
-          #args 0 is the username
-        document.update_cell(row_where_to_add, 1, str(args[0]))
-        #args 1 is the user id
-        document.update_cell(row_where_to_add, 2, str(args[1]))
-        document.update_cell(row_where_to_add, 3,0)
-        document.update_cell(row_where_to_add, 4,0)
-        document.update_cell(row_where_to_add, 5, str('=IF(D' + str(row_where_to_add) + '=0, "Perfect Run or has not yet played a game", C' + str(row_where_to_add)+ "/" + "D"+ str(row_where_to_add) + ")"))
-        return str(args[0]) + " has joined the league"
+    row_where_to_add = document.max_row + 1
+    max_height = document.max_row
+    for i in range(2, max_height + 1):
+        if (document.cell(row=i, column=1).value == args[0]):
+            return "You are already in the league, you cannot join again"
+
+    document.cell(row=row_where_to_add, column=1).value = args[0]
+    document.cell(row=row_where_to_add, column=2).value = str(args[1])
+    document.cell(row=row_where_to_add, column=3).value = 0
+    document.cell(row=row_where_to_add, column=4).value = 0
+    document.cell(row=row_where_to_add, column=5).value = str(
+        '=IF(D' + str(row_where_to_add) + '=0, "Perfect Run or has not yet played a game", C' + str(
+            row_where_to_add) + "/" + "D" + str(row_where_to_add) + ")")
+    workFile.save("Stats.xlsx")
+    return args[0] + " has joined the league"
 
 def listLeagueMembers():
-    fh=open("members.txt","r")
-    final_str="Here are the current members:\n"+fh.read()
-    fh.close()
-    
-    return final_str
+    list=""
+    for i in range(2,document.max_row+1):
+        if(str(document.cell(row=i,column=1).value) != "None"):
+            list+=str(document.cell(row=i,column=1).value)+"\n"
+    return "The current list of members is :\n"+list
+
+print(listLeagueMembers())
 
 def listCommands():
     return "command_list = Here are the following commands:\n!falcon: {\nrank\nmembers\njoin\n'win vs @member'\n'loss vs @member'\nrecord\n}"
